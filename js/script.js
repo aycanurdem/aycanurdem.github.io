9b082023b1e4b7e642ed4f410bac304d7ab71dfc
aycanurdem <Aycan.Demirci@ogr.gidatarim.edu.tr>
2023-08-24 17:46:32 +0300

    deneme

---
+++ js/script.js
@@ -0,0 +1,152 @@
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
+$(document).ready(function() {
+	const $app = $('.app');
+	const $img = $('.app__img');
+	const $pageNav1 = $('.pages__item--1');
+	const $pageNav2 = $('.pages__item--2');
+	let animation = true;
+	let curSlide = 1;
+	let scrolledUp, nextSlide;
+	
+	let pagination = function(slide, target) {
+		animation = true;
+		if (target === undefined) {
+			nextSlide = scrolledUp ? slide - 1 : slide + 1;
+		} else {
+			nextSlide = target;
+		}
+		
+		$('.pages__item--' + nextSlide).addClass('page__item-active');
+		$('.pages__item--' + slide).removeClass('page__item-active');
+		
+		$app.toggleClass('active');
+		setTimeout(function() {
+			animation = false;
+		}, 3000)
+	}
+	
+	let navigateDown = function() {
+		if (curSlide > 1) return;
+		scrolledUp = false;
+		pagination(curSlide);
+		curSlide++;
+	}
+
+	let navigateUp = function() {
+		if (curSlide === 1) return;
+		scrolledUp = true;
+		pagination(curSlide);
+		curSlide--;
+	}
+
+	setTimeout(function() {
+		$app.addClass('initial');
+	}, 1500);
+
+	setTimeout(function() {
+		animation = false;
+	}, 4500);
+	
+	$(document).on('mousewheel DOMMouseScroll', function(e) {
+		var delta = e.originalEvent.wheelDelta;
+		if (animation) return;
+		if (delta > 0 || e.originalEvent.detail < 0) {
+			navigateUp();
+		} else {
+			navigateDown();
+		}
+	});
+
+	$(document).on("click", ".pages__item:not(.page__item-active)", function() {
+		if (animation) return;
+		let target = +$(this).attr('data-target');
+		pagination(curSlide, target);
+		curSlide = target;
+	});
+});
+
+const card = document.querySelector(".card");
+const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+const THRESHOLD = 15;
+
+/*
+ * Read the blog post here:
+ * https://letsbuildui.dev/articles/a-3d-hover-effect-using-css-transforms
+ */
+function handleHover(e) {
+  const { clientX, clientY, currentTarget } = e;
+  const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+  const horizontal = (clientX - offsetLeft) / clientWidth;
+  const vertical = (clientY - offsetTop) / clientHeight;
+  const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+  const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+  card.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+}
+
+function resetStyles(e) {
+  card.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+}
+
+if (!motionMatchMedia.matches) {
+  card.addEventListener("mousemove", handleHover);
+  card.addEventListener("mouseleave", resetStyles);
+}
+
+
+
+$(document).ready(function() {
+	$("#external-content").load("../../cardthing/index.html");
+  });
+  
+
+  (function () {
+	const second = 1000,
+		  minute = second * 60,
+		  hour = minute * 60,
+		  day = hour * 24;
+  
+	//I'm adding this section so I don't have to keep updating this pen every year :-)
+	//remove this if you don't need it
+	let today = new Date(),
+		dd = String(today.getDate()).padStart(2, "0"),
+		mm = String(today.getMonth() + 1).padStart(2, "0"),
+		yyyy = today.getFullYear(),
+		nextYear = yyyy,
+		dayMonth = "09/07/",
+		birthday = dayMonth + yyyy;
+	
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+	  birthday = dayMonth + nextYear;
+	}
+	//end
+	
+	const countDown = new Date(birthday).getTime(),
+		x = setInterval(function() {    
+  
+		  const now = new Date().getTime(),
+				distance = countDown - now;
+  
+		  document.getElementById("days").innerText = Math.floor(distance / (day)),
+			document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+			document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+		  //do something later when date is reached
+		  if (distance < 0) {
+			document.getElementById("headline").innerText = "Yarışma Başladı!";
+			document.getElementById("countdown").style.display = " ";
+			document.getElementById("content").style.display = " ";
+			clearInterval(x);
+		  }
+		  //seconds
+		}, 0)
+	}());
